178dd1dc10da90fc8797e559f410f28a9240b37a
nazimboudeffa <boudeffa@gmail.com>
2022-12-31 17:04:27 +0100

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+//import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
